7a3a0c88c751c5940e33c491441b4999f10cf3e7
pikiavila <wowaura833@gmail.com>
2023-04-21 22:56:36 -0300

    v-mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens:{
+        'xsm': {'min': '320px', 'max': '639px'}
+      },
       backgroundImage: {
         'fondo-naruto':"url('/src/assets/images/7d0eea20d6c53007b24b7e2c4342b8191.png')",
+        'fondo-naruto2': "url('/src/assets/images/naruto-largo.png')",
         'fondo-luffy':"url('/src/assets/images/Rectangle14.png')"  
       }
     }
