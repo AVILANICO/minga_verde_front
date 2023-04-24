bf655afb23883ed38cb359c381d88566852b897f
pikiavila <wowaura833@gmail.com>
2023-04-24 14:03:03 -0300

    v1

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 let apiUrl = 'http://localhost:8000'
 
 if (process.env.NODE_ENV==='production') {
-    apiUrl = process.env.REACT_APP_API
+    apiUrl = import.meta.env.VITE_API
 }
 
 export default apiUrl
