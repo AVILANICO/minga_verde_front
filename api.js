90ac0abc5b0bff99b7315f4e5b8274d0a72b7bf4
pikiavila <wowaura833@gmail.com>
2023-04-24 15:40:03 -0300

    v4

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
-let apiUrl = 'http://localhost:8000/'
+let VITE_API = 'http://localhost:8000/'
 
 if (process.env.NODE_ENV==='production') {
-    apiUrl = import.meta.env.VITE_API
+    VITE_API = import.meta.env.VITE_API
 }
 
-export default apiUrl
+export default VITE_API
