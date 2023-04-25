f3c9f7961acf7ca3f0476c6c7af1a0cd0a798886
pikiavila <wowaura833@gmail.com>
2023-04-25 02:21:51 -0300

    butoncito

--- src/components/FlechaDerecha.jsx
+++ src/components/FlechaDerecha.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function FlechaDerecha() {
   return (
     <>
-      <div className="w-10 self-center">
+      <button className="w-10 self-center">
         <svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF80" viewBox="0 0 24 24" stroke-width=".5" stroke="currentColor" class="w-10 h-10">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-      </div>
+      </button>
     </>
   )
 }
