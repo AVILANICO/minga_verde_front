5b05118f022b47cb0ad709724be66f940eeb7783
AVILANICO <122701169+AVILANICO@users.noreply.github.com>
2023-05-10 11:41:39 -0300

    M 08 (#28)
    * singin/up (#10)
    
    * M 01 (#11)
    
    * front terminado - datos ingresados por el usuario se guardan en el back
    
    * validaciones de usario
    
    * Signin-Signup
    
    * M 04 (#13)
    
    * chapters
    
    * chapters front
    
    * chapters
    
    * Create front Mangaform and routes (#12)
    
    * roles y alertas (#15)
    
    * M 04 roles (#16)
    
    * roles y alertas
    
    * corrigiendo errores de alertas
    
    * modificando alerts
    
    * authform listo (#17)
    
    * edit merges (#18)
    
    * fix-mobile (#19)
    
    * alertas (#20)
    
    * M 03 f (#21)
    
    * edit merges
    
    * edit
    
    * fix mobile XD (#22)
    
    * finally
    
    * sprint2
    
    * fixeando cosillas
    
    * M 01 new (#24)
    
    * fixeando cosillas
    
    * fixeando ultimos detalles
    
    * paginacion
    
    ---------
    
    Co-authored-by: Luis Hernandez <125385157+LuisH05@users.noreply.github.com>
    Co-authored-by: JoelDamato <127883956+JoelDamato@users.noreply.github.com>
    Co-authored-by: Joeldamato <damatojoel25@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import routers from './router/router.jsx'
+// import { Provider } from 'react-redux'
+// import store from './store/store.jsx'
 
-//el main.jsx contiene la logica necesaria para seleccionar al div root y crear/renderizar una app de REACT.
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={routers} />
-  </React.StrictMode>,
+  // <Provider store={store}>
+  <RouterProvider router={routers} />,
+  // </Provider>
 )
+
+//el enrutador principal tiene que ser hijo del almacenamiento de estados globales
+//para que cada interfaz del enrutador pueda acceder a los estados globales de redux
